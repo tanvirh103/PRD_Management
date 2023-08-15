8c34f82aca6241c5829d049619cf41ef23e5c4bc
tanvir103 <tanvirh103@gmail.com>
2023-08-15 17:10:15 +0600

    .

---
+++ analyst_dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analyst Dashboard</title>
+</head>
+<body>
+    <center>
+    <font face="times new roman" size="8">Analyst Dashboard</font>
+    <br><br>
+        <table>
+            <tr><td><a href="createProject.html">Create Your Project</a></td></tr>
+            <tr><td><a href="createFeature.html">Create Feature</a></td></tr>
+            <tr><td><a href="createSpecification.html">Create Specification</a></td></tr>
+            <tr><td><a href="View_all_projecthtml">View All Project</a></td></tr>
+            <tr align="right"><td><br><br><br>&nbsp;&nbsp;&nbsp;&nbsp;<a href="logout.php">Logout</a></td></tr>
+        </table>
+    </center>
+</body>
+</html>
---
+++ login_controller.php
@@ -0,0 +1,13 @@
+<?php
+require_once('user_model.php');
+if(isset($_POST['submit'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $result=userinfo($username,$password);
+    if(mysqli_num_rows($result)>0){
+        header('location:analyst_dashboard.php');
+    }else{
+        header('location:wrong.html');
+    }
+}
+?>
