3bfc45ecd436ca75e168a51c761117b6181c0547
tanvir103 <tanvirh103@gmail.com>
2023-08-16 03:36:18 +0600

    .

--- feature_controller.php
+++ feature_controller.php
@@ -1,6 +1,17 @@
 <?php
-    if(isset($_POST['subbmit'])){
-        $projectname=$_POST['projectname'];
+    require_once('user_model.php');
+
+    if(isset($_POST['submit'])){
+        $projectname=$_POST['ProjectName'];
         $featurename=$_POST['featurename'];
+        
+        $row=projectid($projectname);
+        $PID=$row['PID'];
+        $result=addFeature($PID,$featurename);
+        if($result){
+            echo"<font face=\"times new roman\" size=\"6\">Congratulations New Feature Has been added</font>";
+        }else{
+            echo"<font face=\"times new roman\" size=\"6\">Failed! Please try again</font>";
+        }
     }
 ?>
--- user_model.php
+++ user_model.php
@@ -12,4 +12,23 @@ function addProject($projectname,$projectdomain,$projectdescription){
     $result=mysqli_query($conn,$sql);
     return $result;
 }
+function projectname(){
+    $conn=dbConnection();
+    $sql="SELECT ProjectName FROM projectinfo";
+    $result=mysqli_query($conn,$sql);
+    return $result;
+}
+function projectid($ProjectName){
+    $conn=dbConnection();
+    $sql="SELECT * FROM projectinfo where ProjectName='$ProjectName'";
+    $result=mysqli_query($conn,$sql);
+    $row=mysqli_fetch_assoc($result);
+    return $row;
+}
+function addFeature($PID,$featurename){
+    $conn=dbConnection();
+    $sql="INSERT INTO featureinfo VALUES('','$PID','$featurename')";
+    $result=mysqli_query($conn,$sql);
+    return $result;
+}
 ?>
